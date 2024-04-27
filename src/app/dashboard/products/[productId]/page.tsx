59ba34e905b8472a4019f05f031f657be82ee088
Pulkit Gupta <hiiampulkitg@gmail.com>
2024-04-27 16:35:50 +0530

    feat: dynamic routes of product

--- src/app/dashboard/products/[productId]/page.tsx
+++ src/app/dashboard/products/[productId]/page.tsx
@@ -1,9 +1,5 @@
-'use client';
-import { useRouter } from 'next/router';
-
-export default function Pid() {
-  const router = useRouter();
-  const { pid } = router.query;
-
-  return <div className="mx-auto max-w-5xl px-8">Hello, {pid}</div>;
+export default function Pid({ params }: { params: any }) {
+  return (
+    <div className="mx-auto max-w-5xl px-8">Hello, {params.productId}</div>
+  );
 }
--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,39 +1,39 @@
 import { HoverEffect } from '@/components/ui/card-hover-effect';
 
-async function getProjects() {
+async function getProducts() {
   return [
     {
-      title: 'Project 1',
+      title: 'Product 1',
       imageLink: 'https://via.placeholder.com/1080x900',
       id: 1,
     },
     {
-      title: 'Project 2',
+      title: 'Product 2',
       imageLink: 'https://via.placeholder.com/1080x900',
       id: 2,
     },
     {
-      title: 'Project 3',
+      title: 'Product 3',
       imageLink: 'https://via.placeholder.com/1080x900',
       id: 3,
     },
     {
-      title: 'Project 4',
+      title: 'Product 4',
       imageLink: 'https://via.placeholder.com/1080x900',
       id: 4,
     },
     {
-      title: 'Project 5',
+      title: 'Product 5',
       imageLink: 'https://via.placeholder.com/1080x900',
       id: 5,
     },
   ];
 }
 export default async function product() {
-  const projects = await getProjects();
+  const Products = await getProducts();
   return (
     <div className="mx-auto max-w-5xl px-8">
-      <HoverEffect items={projects} />
+      <HoverEffect items={Products} />
     </div>
   );
 }
