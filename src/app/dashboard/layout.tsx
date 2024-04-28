9d1225f913a01f3f3f349e5981c7aee32484d407
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-04-28 06:07:48 +0530

    fix: correct z-index

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({
       <div className="fixed inset-0 overflow-hidden">
         <div className=" grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
           <SideBarWide />
-          <div className="z-50 flex h-14 flex-col overflow-auto">
+          <div className="flex h-14 flex-col overflow-auto">
             <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
               <SideBarMobile />
               <div className="w-full flex-1">
@@ -27,7 +27,7 @@ export default function DashboardLayout({
       <div className="flex overflow-hidden">
         <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
           <div className="hidden border-r bg-muted/40 md:block"></div>
-          <main className="z-0 flex flex-1 flex-col gap-4 overflow-auto pt-16 lg:gap-6">
+          <main className="flex flex-1 flex-col gap-4 overflow-auto pt-16 lg:gap-6">
             {children}
           </main>
         </div>
