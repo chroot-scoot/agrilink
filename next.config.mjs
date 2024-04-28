4e285f503d18ff62b32b7771d84d60aa5ae6972a
pulkit4603 <pulkitdwivedi123@gmail.com>
2024-04-28 07:19:06 +0530

    feat: landing page

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,17 @@ const jiti = createJITI(new URL(import.meta.url).pathname);
 jiti('./src/env');
 
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'khsvweruovpppinstuvf.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/se-hack-screenshots/**',
+      },
+    ],
+  },
+};
 
 export default config;
